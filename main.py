9ee0f2e5fe119c8ffa322235b6c14ecde482ad52
Asher Zaczepinski <asherzac2020@gmail.com>
2024-06-12 18:08:16 -0700

    removeddiagonalupcheck

--- main.py
+++ main.py
@@ -105,33 +105,7 @@ def process_line(input_y, img_array, width, difference_between_lines_for_line_dr
                                     break
                                 temp_y_below += 1
                             if below_flag == False:
-                                white_note = False
-                    #this will do the up right thing after determining everything else works 
-                    if white_note:
-                        up = 0
-                        up_right = 0
-                        counter = 1
-                        while True:
-                            temp_pixel_0 = input_y - up
-                            temp_pixel_1 = x_index - difference_between_blacks - 1 + up_right
-                            temp_pixel = img_array[temp_pixel_0, temp_pixel_1]
-                            if temp_pixel_0 <= input_y - difference_between_lines / 2 or temp_pixel_1 > x_index - (difference_between_blacks / 2) - 1:
-                                break
-                            if temp_pixel == 255:
-                                white_note = False
-                                break
-                            right_addend = 0
-                            while True:
-                                if temp_pixel_1 + right_addend >= width:
-                                    white_note = False
-                                    break
-                                new_pixel = img_array[temp_pixel_0 - 1, temp_pixel_1 + right_addend]                                    
-                                if new_pixel == 255:
-                                    break
-                                right_addend += 1
-                            up += 1
-                            up_right += right_addend - 1
-                            counter += 1
+                                white_note = False                    
                     #making sure there is not too much variation
                     if white_note:
                         past_temp_y_above = -1
